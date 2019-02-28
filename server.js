6b5528ef1ed257e78a6de0cce244a1326adea331
rachelsue <wells.rachelsue@gmail.com>
2019-02-27 20:28:59 -0600

    heroku

--- server.js
+++ server.js
@@ -31,6 +31,7 @@ var MONGODB_URI =
   process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGODB_URI);
 
+
 // Routes
 app.get("/", function() {
   res.sendFile(path.join(__dirname, "..public/index.html"));
